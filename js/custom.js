e7c2c87fe1a60e92156b8795c1d0be4a14283d1f
Kris <2193578t@student.gla.ac.uk>
2018-11-17 20:04:20 +0000

    Expand toggle only show on collapse

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,11 @@ jQuery(document).ready($=>{
     const sidebar = $('nav.sidebar');
     $('#collapse').click(()=>{
         sidebar.css("width", "0%");
-        $('#expand').css("display", "block");
+        $('#expand').css("opacity", "1");
     });
     $('#expand').click(e=>{
         sidebar.css("width", "15%");
-        $(this).css("display", "none");
+        $('#expand').css("opacity", "0");
         console.log($(this).css("background", "red"));
     })
 });
