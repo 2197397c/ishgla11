03ad0d255841d08306e40a0af356075e1cdbe5fc
Kris <2193578t@student.gla.ac.uk>
2018-11-23 11:05:20 +0000

    Added XML2JSON Library | Added JSON File Onload

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,6 @@
+let x2js = new X2JS();
+// let textFile = readTextFile("test.txt");
+// console.log(textFile);
 jQuery(document).ready($=>{
     const sidebar = $('nav.sidebar');
     $('#collapse').click(()=>{
@@ -7,5 +10,20 @@ jQuery(document).ready($=>{
     $('#expand').click(e=>{
         sidebar.css("width", "15%");
         $('#expand').css("opacity", "0");
-    })
+    });
+    let gpxData;
+    $.ajax({
+        url: 'map/Activities/activity_1927428247.gpx',
+        async: false,
+        cache: false,
+        dataType: 'text',
+        success: e => {
+            // console.log(e);
+            gpxData = e;
+        },
+        error: e => console.log(e)
+    });
+    // console.log(gpxData);
+    let jsonObj = x2js.xml_str2json(gpxData);
+    console.log(jsonObj);
 });
