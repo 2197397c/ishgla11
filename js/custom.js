0414c86b22f159720cfab289bab02a96d8b0b824
Kris <2193578t@student.gla.ac.uk>
2018-11-26 13:26:51 +0000

    Sidebar control is now one button | Supported jQuery navbar state | screen size support

--- js/custom.js
+++ js/custom.js
@@ -3,11 +3,22 @@ jQuery(document).ready($=>{
     // const mymap = L.map('map');
     $('#collapse').click(()=>{
         sidebar.css("width", "0%");
-        $('#expand').css("opacity", "1");
+        // $('#expand').css("opacity", "1");
     });
     $('#expand').click(e=>{
-        sidebar.css("width", "15%");
-        $('#expand').css("opacity", "0");
+        // sidebar.css("width", "15%");
+        let state = $(e.target).data("state");
+        if (state === "on"){
+            sidebar.css("width", "0%");
+            $(e.target).data("state", "off");
+            $("i", e.target).toggleClass("fa-chevron-left").toggleClass("fa-chevron-right");
+        } else {
+            sidebar.css("width", "var(--sidebar-width)");
+            $(e.target).data("state", "on");
+            $("i", e.target).toggleClass("fa-chevron-left").toggleClass("fa-chevron-right");
+        }
+        // console.log($(e.target).data("state"));
+        // $('#expand').css("opacity", "0");
     });
     $('#add').click(e=>{
         $('#addAnimal').css("display", "flex");
